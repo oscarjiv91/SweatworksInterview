a2cd6043a6525d1f6fdd59a7374523ca2be7f6e9
Oscar J. Irun <oscarjiv91@gmail.com>
2017-02-17 16:39:55 -0300

    Solved

--- controller.js
+++ controller.js
@@ -1,4 +1,36 @@
 angular.module('test', [])
 .controller('testController', ['$scope', function($scope) {
-	
-}]);
+
+	$scope.numbersList = []
+
+	$scope.generateArray = function(){
+		$scope.numbersList = []
+		if($scope.minNumber > $scope.maxNumber){
+			$scope.message = "Min number is higher than Max number. Try again."
+		}else{
+			$scope.message = null
+			for(x = $scope.minNumber; x <= $scope.maxNumber; x++){
+				$scope.numbersList.push(x)
+			}
+		}
+	}
+
+	$scope.filterByOption = function(item){
+		switch ($scope.option) {
+			case "EVEN":
+				return item % 2 ? null : item
+			case "ODD":
+				return item % 2 ? item : null
+			default:
+				return item
+		}
+	}
+}])
+
+.directive('numbers', function() {
+  return {
+      restrict: 'AE',
+      replace: 'true',
+      template: '<div ng-repeat="number in numbersList | filter: filterByOption"> {{number}} </div>'
+  };
+});
